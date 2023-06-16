333b900802d9b82cdf7b5e2208a135c20d4dd5b2
Jonas Friederich <33685239+jo-chr@users.noreply.github.com>
2023-06-16 14:29:22 +0200

    Update ModelValidator.py

--- ModelValidator.py
+++ ModelValidator.py
@@ -15,17 +15,20 @@ except:
 import numpy as np
 import copy
 import statistics
+import datetime
 import scipy.stats as st
 
 from ModelManipulator import ModelManipulator
 
 class ModelValidator:
 
-    def __init__(self, spn:SPN, time_unit):
+    def __init__(self, spn:SPN, time_unit, event_log, state_log):
         self.spn = spn
         self.time_unit = time_unit
+        self.event_log = event_log
+        self.state_log = state_log
 
-    def input_output_transformation(self, nr_replications, time, results_transition, ground_truth, warm_start, warm_start_time):
+    def input_output_transformation(self, nr_replications, time, results_transition, warm_start, warm_start_time):
         
         y_r = []
         for i in range(nr_replications):
@@ -47,7 +50,6 @@ class ModelValidator:
         print("Simulation results: {}".format(y_r))
         y_mean = np.mean(y_r)
         print("Mean: {}".format(y_mean))
-        print("Ground Truth: {}".format(ground_truth))
         y_variance = round(statistics.variance(y_r),2)
         print("Variance: {}".format(y_variance))
         y_stdev = round(statistics.stdev(y_r),2)
@@ -56,4 +58,33 @@ class ModelValidator:
         ci = st.t.interval(alpha=0.95,df=len(y_r)-1, loc=np.mean(y_r),scale=st.sem(y_r))
         print("[{}, {}]".format(round(ci[0],2),round(ci[1],2)))
 
+        ground_truth, gt_mean, gt_ci = self.calculate_ground_truth(time, mode = "order_completed")
+        print("Ground Truth: {}".format(ground_truth))
+        print("Ground Truth Mean: {}".format(gt_mean))
+        print("Ground Truth CI: {}".format(gt_ci))
+
         return ci, y_mean
+
+    def calculate_ground_truth(self, time, mode):
+
+        el_total_time = self.event_log["timestamp"][len(self.event_log["timestamp"]) - 1] - self.event_log["timestamp"][0]
+        time_multiplier = el_total_time.seconds // time
+
+        start_time = self.event_log["timestamp"][0]
+
+        ground_truth = []
+
+        for i in range(time_multiplier):
+            end_time = start_time + datetime.timedelta(seconds=time)
+            event_log_subset = self.event_log[(self.event_log['timestamp'] >= start_time) & (self.event_log['timestamp'] <= end_time)]
+            start_time = end_time
+
+            if mode == "order_completed":
+                ground_truth.append(len(event_log_subset["event"][event_log_subset["event"]=="order_completed"]))
+
+        ground_truth
+        gt_mean = np.mean(ground_truth)
+        gt_ci = st.t.interval(alpha=0.95,df=len(ground_truth)-1, loc=np.mean(ground_truth),scale=st.sem(ground_truth))
+
+        return ground_truth, gt_mean, gt_ci
+
